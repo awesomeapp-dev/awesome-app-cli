0e131edcfc5cf35df9931cb4d52ad0b3ffd397b6
Jeremy Chone <jeremy.chone@gmail.com>
2022-09-28 13:35:43 -0700

    . update dependencies (clap 4.0)

--- src/cmd_app.rs
+++ src/cmd_app.rs
@@ -2,7 +2,7 @@ use clap::{crate_version, Arg, Command};
 
 pub const VERSION: &str = &crate_version!();
 
-pub fn cmd_app() -> Command<'static> {
+pub fn cmd_app() -> Command {
 	Command::new("awesome-app")
 		.version(&VERSION[..])
 		.about("Awesome Desktop App Scaffolder")
@@ -10,12 +10,12 @@ pub fn cmd_app() -> Command<'static> {
 		.subcommand(sub_dev())
 }
 
-fn sub_new() -> Command<'static> {
+fn sub_new() -> Command {
 	Command::new("new")
 		.about("Build new tauri app from template base")
 		.arg(Arg::new("name").help("App/project name (no space)"))
 }
 
-fn sub_dev() -> Command<'static> {
+fn sub_dev() -> Command {
 	Command::new("dev").about("Starts hot-reload developement")
 }
